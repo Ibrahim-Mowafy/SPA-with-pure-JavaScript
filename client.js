87cc082ea79d5f43b3c89eb3c4c3f3d3a20f86ab
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2022-12-12 12:12:20 +0200

    Add sign up/login form with email

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 const listOfVidReqElm = document.getElementById('listOfRequests');
-let sortBy = 'newFirst';
-let searchTerm = '';
+
+const state = {
+  sortBy: 'newFirst',
+  searchTerm: '',
+  userId: '',
+};
 
 function renderSingleVidReq(vidInfo, appended = false) {
   const vidReqContainerElm = document.createElement('div');
@@ -88,20 +92,9 @@ function loadAllVidReq(sortBy = 'newFirst', searchTerm = '') {
 }
 
 function checkValidity(formData) {
-  const name = formData.get('author_name');
-  const email = formData.get('author_email');
   const topic = formData.get('topic_title');
   const topicDetails = formData.get('topic_details');
 
-  if (!name) {
-    document.querySelector('[name=author_name]').classList.add('is-invalid');
-  }
-  const emailPattern =
-    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-
-  if (!email || !emailPattern.test(email)) {
-    document.querySelector('[name=author_email]').classList.add('is-invalid');
-  }
   if (!topic || topic.length > 30) {
     document.querySelector('[name=topic_title]').classList.add('is-invalid');
   }
@@ -137,17 +130,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const sortByElms = document.querySelectorAll('[id*=sort_by_]');
   const searchBoxElm = document.getElementById('search_box');
 
+  const formLoginElm = document.querySelector('.form-login');
+  const appContentElm = document.querySelector('.app-content');
+
+  if (window.location.search) {
+    state.userId = new URLSearchParams(window.location.search).get('id');
+    formLoginElm.classList.add('d-none');
+    appContentElm.classList.remove('d-none');
+  }
+
   loadAllVidReq();
 
   sortByElms.forEach((elm) => {
     elm.addEventListener('click', function (e) {
       e.preventDefault();
-      sortBy = this.querySelector('input').value;
-      loadAllVidReq(sortBy, searchTerm);
+      state.sortBy = this.querySelector('input').value;
+      loadAllVidReq(state.sortBy, state.searchTerm);
 
       this.classList.add('active');
 
-      if (sortBy === 'topVotedFirst') {
+      if (state.sortBy === 'topVotedFirst') {
         document.getElementById('sort_by_new').classList.remove('active');
       } else {
         document.getElementById('sort_by_top').classList.remove('active');
@@ -158,8 +160,8 @@ document.addEventListener('DOMContentLoaded', () => {
   searchBoxElm.addEventListener(
     'input',
     debounce((e) => {
-      searchTerm = e.target.value;
-      loadAllVidReq(sortBy, searchTerm);
+      state.searchTerm = e.target.value;
+      loadAllVidReq(state.sortBy, state.searchTerm);
     }, 300)
   );
 
@@ -167,6 +169,7 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     const formData = new FormData(fromVidReqElm);
 
+    formData.append('author_id', state.userId);
     const isValid = checkValidity(formData);
     if (!isValid) return;
 
